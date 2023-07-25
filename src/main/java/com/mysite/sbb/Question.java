52eb1cf35adf7c2e420a789c460e98fd31c3bca8
parkchaewon18 <0101angela@naver.com>
2023-07-25 22:01:26 +0900

    Feat: entity

---
+++ src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,30 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id //PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200) //VARCHAR(200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    //실제 DB 테이블에 칼럼이 생성되지 않는다. -> DB는 배열이나 리스트를 칼럼에 저장할 수 없기 때문
+    //CascadeType.REMOVE : 질문을 삭제하면 그에 달린 답변들도 모두 함께 삭제하기 위해서
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
