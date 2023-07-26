825822cff00ffb10e33c461ce1b2427b13e67797
parkchaewon18 <0101angela@naver.com>
2023-07-26 15:41:47 +0900

    Feat: Repository-Data Save, Data Read

--- db/init.sql
+++ db/init.sql
@@ -2,3 +2,13 @@
 DROP DATABASE IF EXISTS sbb;
 CREATE DATABASE sbb;
 USE sbb;
+
+## 외래키 제약 끄기
+SET FOREIGN_KEY_CHECKS = 0;
+
+#각 테이블 초기화 //answer 먼저 삭제
+TRUNCATE answer;
+TRUNCATE question;
+
+# 외래키 제약 복구
+SET FOREIGN_KEY_CHECKS = 1;
