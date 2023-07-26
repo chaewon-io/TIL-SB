2a542fe88cf54c10eabc6f9443f785ea1572bb2b
parkchaewon18 <0101angela@naver.com>
2023-07-26 16:19:49 +0900

    Feat: Repository-Test Case Independent

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,15 @@ class SbbApplicationTests {
 	@Autowired //자동연결
 	private QuestionRepository questionRepository;
 
-	@Test
-	@DisplayName("데이터 저장")
-	void t001() {
+	@BeforeEach
+		// 아래 메서드는 각 테스트케이스가 실행되기 전에 실행된다.
+	void beforeEach() {
+		// 모든 데이터 삭제
+		questionRepository.deleteAll();
+
+		// 흔적삭제(다음번 INSERT 때 id가 1번으로 설정되도록)
+		questionRepository.clearAutoIncrement();
+
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
@@ -33,6 +40,12 @@ class SbbApplicationTests {
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
 		questionRepository.save(q2);  // 두번째 질문 저장
+	}
+
+	@Test
+	@DisplayName("데이터 저장")
+	void t001() {
+
 	}
 
 	@Test
@@ -100,9 +113,5 @@ class SbbApplicationTests {
 		questionRepository.delete(q);
 		assertEquals(1, questionRepository.count());
 	}
-
-
-
-
 
 }
