216bae6bed0e6aa612dad713d5e247993a68625f
parkchaewon18 <0101angela@naver.com>
2023-07-27 16:24:47 +0900

    Feat: Answer-Save, Display

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -3,6 +3,7 @@ package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
+import com.zaxxer.hikari.metrics.MetricsTrackerFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class AnswerController {
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable Integer id, @RequestParam String content) {
@@ -23,6 +25,7 @@ public class AnswerController {
         Question question = questionService.getQuestion(id);
 
         // TODO : 답변객체
+        Answer answer = answerService.create(question, content);
 
         return "redirect:/question/detail/%d".formatted(id);
     }
