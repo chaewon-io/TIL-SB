46d1e0c0fd0be5ee00996e7b37fbbeeb3f0be7b6
parkchaewon18 <0101angela@naver.com>
2023-07-28 19:03:31 +0900

    Feat: Answer Form

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -4,9 +4,11 @@ package com.mysite.sbb.answer;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
 import com.zaxxer.hikari.metrics.MetricsTrackerFactory;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +22,18 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable Integer id, @RequestParam String content) {
+    public String createAnswer(Model model, @PathVariable Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
+
         // 관련 질문을 얻어온다.
         Question question = questionService.getQuestion(id);
 
-        // TODO : 답변객체
-        Answer answer = answerService.create(question, content);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+
+        // 답변객체
+        Answer answer = answerService.create(question, answerForm.getContent());
 
         return "redirect:/question/detail/%d".formatted(id);
     }
