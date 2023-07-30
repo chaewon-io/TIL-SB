26995be6bf24886920116e5d31989795a1c6cdae
parkchaewon18 <0101angela@naver.com>
2023-07-30 23:18:20 +0900

    Feat: N+1 Solution

--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -30,6 +32,7 @@ public class Question {
     //실제 DB 테이블에 칼럼이 생성되지 않는다. -> DB는 배열이나 리스트를 칼럼에 저장할 수 없기 때문
     //CascadeType.REMOVE : 질문을 삭제하면 그에 달린 답변들도 모두 함께 삭제하기 위해서
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    @LazyCollection(LazyCollectionOption.EXTRA) // answerList.size(); 함수가 실행될 때 SELECT COUNT 실행
     //OneToMany는 직접 객체 초기화
     private List<Answer> answerList = new ArrayList<>();
 
