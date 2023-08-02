9d2d666f63338a38db063593369e481279ba27b4
parkchaewon18 <0101angela@naver.com>
2023-08-02 11:30:23 +0900

    Feat: Question Modify and Delete & Answer Modify

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -49,4 +49,14 @@ public class QuestionService {
         return q;
 
     }
+    public void modify(Question question, String subject, String content) {
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setModifyDate(LocalDateTime.now());
+        questionRepository.save(question);
+    }
+
+    public void delete(Question question) {
+        questionRepository.delete(question);
+    }
 }
